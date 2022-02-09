ddc0372f2450839cc372e892bf0deca05ae8259e
Lucas <lucasbsb100@gmail.com>
2022-02-09 16:02:19 -0300

    Add atualizaçao do cliente no arquivo
    Co-authored-by: Lucas Rodrigues   <lucasro229@gmail.com>
    Co-authored-by: Kleidson Alves    <kleidsonalves15@gmail.com>

--- edit_data.py
+++ edit_data.py
@@ -1,25 +1,13 @@
+
 def main():
     id_client = identify_client()
-    presentation_data(id_client)
+    # init, final = search_client(file, id_client)
+    client = read_client_file(id_client)
+    presentation_data(client)
     option = edit_client_menu()
-    edit_client(option, id_client)
+    edit_client(option, client)
 
-def edit_client_menu():
-    print("Qual dado você gostaria de alterar?")
-    print("1 - Nome:")
-    print("2 - Telefone")
-    return input("Escolha uma opção: ")
- 
-
-def presentation_data(id_client):
-    # Uso de Programação OO
-    file = open("./tmp/clients.txt", "r")
-    client = search_client(file, id_client)
-    file.close()
-    #
-    # print(f"CPF: {client['cpf']}\nNome: {client['name']}\nTelefone: {client['phone']}")
-
-
+# Funções que substituem métodos python
 def split(line, char=",", i=0):
     if i >= len(line):
         return [line[:-1]]
@@ -28,30 +16,95 @@ def split(line, char=",", i=0):
     else:
         return split(line, char, i+1)
     
-
-def search_client(file, client):
-    init = file.tell()
-    line = split(file.readline())
-    final = file.tell()
-
-    if client in line:
-        return init, final
+def join(string_list, i=0):
+    if i >= len(string_list):
+        return ""
     
-    return search_client(file, client)
+    txt_join = string_list[i]
+    return txt_join + join(string_list, i+1)
+
+## Funções de arquivo
+
+def read_clients_file():
+    file = open("./tmp/clients.txt", "r")
+    lines = file.readlines()
+    file.close()
+
+    return lines
+
+def read_client_file(id_client):
+    clients = read_clients_file()   
+    index = search_client(clients, id_client)
+    if index == -1:
+        print('Cliente não cadastrado!')
+        exit()         
+    return split(clients[index])
+
+def update_client(clients, index, new_data, i=0, text=""):
+    return clients[:index] + [f"{new_data[0]},{new_data[1]},{new_data[2]}\n"] + clients[index+1:]
+
+
+
+def update_clients_file(id_client, new_data): 
+    clients = read_clients_file()   
+    index = search_client(clients, id_client)
+    file = open("./tmp/clients.txt", "w")
+    list_string = update_client(clients, index, new_data)
+    file.write(join(list_string))
+    file.close()
+
+
+# Edição do cliente 
+
+def edit_client_menu():
+    print("Qual dado você gostaria de alterar?")
+    print("1 - Nome:")
+    print("2 - Telefone")
+    return int(input("Escolha uma opção: "))
+ 
+
+def presentation_data(client):
+    print(f"CPF: {client[0]}\nNome: {client[1]}\nTelefone: {client[2]}")
+
+
+# def search_client(file, id_client, index = 0):
+#     # print(index)
+#     init = file.tell()
+#     line = split(file.readline())
     
+#     if '' in line:
+#         return
+    
+#     if id_client in line:
+#         return init, index
+    
+#     return search_client(file, id_client, index+1)
+
+# 0000,lll
+# 0001,lll
+# 0002,lll
+
+
+def search_client(clients, id_client, index = 0):
+    if index >= len(clients):
+        return -1
+    
+    if id_client in split(clients[index]):
+        return index
+    
+    return search_client(clients, id_client, index+1)
+
+
+
 def edit_client(option, client):
     if option == 1:
-        edit_name(client)
+        new_data = [client[0], input("Digite o novo nome: "), client[2]]
     else:
-        edit_phone(client)
-
-def edit_name(client):
-    ...
-def update_client_data(client):
-    ...
-
-def edit_phone(id_client):
-    ...
+        new_data = [client[0], client[1], input("Digite o novo telefone: ")]
+        
+    update_clients_file(client[0], new_data)
 
 def identify_client():
-    ...
+    return input("Digite o CPF do Cliente: ")
+
+main()
--- register_client.py
+++ register_client.py
@@ -1,12 +1,9 @@
-# def main():
-#     user = getUserData()
-#     register_user(user)
-
 def write_on_file(user):
+    # Uso de Programação OO
     file = open(f"./temp/clients.txt", "a")
-    file.write(f"{user['cpf']}\n{user['name']}\n{user['phone']}\n\n")
+    file.write(f"{user['cpf']}, {user['name']}, {user['phone']}\n")
     file.close()
-    
+    # 
 
 def get_user_data():
     print("Enter yours datas!\n")
@@ -15,5 +12,3 @@ def get_user_data():
     phone = input ('Telefone: ')
     return {"name": name, "cpf": cpf, "phone": phone}
 
-
-# main()
