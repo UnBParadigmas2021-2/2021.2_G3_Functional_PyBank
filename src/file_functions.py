907597ddd23a8a74def2b5bf44999c0ecc18fd06
Samuel Nogueira <samuelnbacelar@hotmail.com>
2022-02-11 15:23:16 -0300

    Criando função de escrever em um arquivo
    Co-authored-by:  Matheus73 <matheuscoding@gmail.com>
    Co-authored-by:  EduardoPicolo <eduardonpicolo@gmail.com>

--- src/file_functions.py
+++ src/file_functions.py
@@ -1,9 +1,15 @@
-def write_on_file(data, file):
+from utils import join
+
 def append_on_file(data, file):
     file = open(file, "a")
     data_extracted = extract_data(data)
     new_object = parse_to_string(data_extracted)
     file.write(new_object)
+    file.close()
+
+def write_on_file(data, file):
+    file = open(file, 'w')
+    file.write(join(data))
     file.close()
 
 def parse_to_string(data, index=0):
@@ -12,8 +18,8 @@ def parse_to_string(data, index=0):
     return data[index] + "," + parse_to_string(data, index + 1)
 
 def extract_data(data):
-    return [data[value] for value in data.keys()]
-    
+    return [data[value] for value in data]
+
 
 def read_from_file(file):
     file = open(file, "r")
