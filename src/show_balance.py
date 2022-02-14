98317a9dd9d283d07e512b5837b9cd553e477237
Lucas <lucasbsb100@gmail.com>
2022-02-14 18:26:35 -0300

    Corrige erro de exibiçao do saldo da conta corrente
    Co-authored-by: Lucas Rodrigues <lucasro229@gmail.com>

--- src/show_balance.py
+++ src/show_balance.py
@@ -4,7 +4,10 @@ from operations import increase_balance, update_balance
 def show_user_balance():
     client_data = identify_client(input("Digite o número da conta: "))
     client_balance, type_account= get_client_data(client_data,["balance", "type"])
-    print(f'\nO seu saldo atual é de: R$ {float(client_balance)*1.01:.2f}\n')
+
     if(type_account == 'poupanca'):
+        print(f'\nO seu saldo atual é de: R$ {float(client_balance)*1.01:.2f}\n')
         print(f'O seu rendimento atualmente é de: R$ {float(client_balance)*0.01:.2f}\n')
         update_balance(client_data, float(client_balance)*0.01, increase_balance)
+    else:
+        print(f'\nO seu saldo atual é de: R$ {client_balance}\n')
