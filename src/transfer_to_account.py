dba181f23c559d4c4db3195673ec6b2784383ff6
EduardoPicolo <eduardonpicolo@gmail.com>
2022-02-14 03:22:58 -0300

    Add error handling to transfer_to_account
    Co-authored-by: Matheus73 <matheuscoding@gmail.com>

--- src/transfer_to_account.py
+++ src/transfer_to_account.py
@@ -5,20 +5,23 @@ from withdraw_from_account import get_withdraw_amount
 
 
 def transfer_to_account():
-    client = identify_client()
+    try:
+        client = identify_client()
 
-    balance, account_number = get_client_data(
-        client, ['balance', 'account_number'])
-    print(f"\tSaldo atual: {format_amount(balance)}")
+        balance, account_number = get_client_data(
+            client, ['balance', 'account_number'])
+        print(f"\tSaldo atual: {format_amount(balance)}")
 
-    print('Por favor, digite o valor que deseja transferir: ', end='')
-    amount = get_withdraw_amount(balance)
+        print('Por favor, digite o valor que deseja transferir: ', end='')
+        amount = get_withdraw_amount(balance)
 
-    print(
-        f'Para quem você deseja transferir a quantia de {format_amount(amount)}:?', end='')
-    target = identify_client()
+        print(
+            f'Para quem você deseja transferir a quantia de {format_amount(amount)}:?', end='')
+        target = identify_client()
 
-    update_balance(client, amount, decrease_balance)
-    update_balance(target, amount, increase_balance)
-
-    print("\nSua transferência foi efetuada com sucesso!\n")
+        update_balance(client, amount, decrease_balance)
+        update_balance(target, amount, increase_balance)
+    except:
+        print(f'Operação cancelada.')
+    else:
+        print("\nSua transferência foi efetuada com sucesso!\n")
