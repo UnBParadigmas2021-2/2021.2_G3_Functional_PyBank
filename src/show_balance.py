22566942271251c65b06358cee4aaf0538b51611
Lucas <lucasbsb100@gmail.com>
2022-02-14 10:50:31 -0300

    Corrigindo erro de cast e salvando rendimento

--- src/show_balance.py
+++ src/show_balance.py
@@ -1,8 +1,10 @@
 from edit_data import identify_client, get_client_data
+from operations import increase_balance, update_balance
 
 def show_user_balance():
     client_data = identify_client(input("Digite o número da conta: "))
-    client_balance, type_account = get_client_data(client_data,["balance", "type"])
-    print(f'\nO seu saldo atual é de: R$ {client_balance}\n')
+    client_balance, type_account= get_client_data(client_data,["balance", "type"])
+    print(f'\nO seu saldo atual é de: R$ {float(client_balance)*1.01:.2f}\n')
     if(type_account == 'poupanca'):
-        print(f'O seu rendimento atualmente é de: R$ {int(client_balance)*0.01}\n')
+        print(f'O seu rendimento atualmente é de: R$ {float(client_balance)*0.01:.2f}\n')
+        update_balance(client_data, float(client_balance)*0.01, increase_balance)
